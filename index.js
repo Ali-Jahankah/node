19a69fb9f33aef510e7ba9bc6b10e6b935fa0541
Ali Jahankah <aral2@yahoo.com>
2022-04-21 18:38:12 +0100

    learning file system

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-const chalk = require('chalk');
-const {
-    greeting,
-    fullName
-} = require('./tester.js');
+const fs = require("fs");
+fs.writeFile("test.txt", "Hello this is a test", (er) => {
+  if (er) throw er;
+  console.log("Data seved!");
+});
+console.log("first console");
+console.log("second console");
+fs.writeFileSync("sync-file-test", "This is a sync file in Node.js");
+fs.appendFile(
+  "./test.txt",
+  "\n This text has append with fs.appendFile",
+  (err) => {
+    if (err) throw err;
+    console.log("Text has appended");
+  }
+);
+fs.appendFileSync("./test.txt", "This is a sync text with fs.appendFileSync");
+fs.unlink("./test.txt", (err) => {
+  if (err) throw err;
+});
+fs.rename("./sync-file-test", "renameToTest2", (err) => {
+  if (err) throw err;
+});
+fs.unlink("./renameToTest2.text", (err) => {
+  if (err) throw err;
+});
+//=================Creating a server====================
 
+// const chalk = require("chalk");
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   console.log(req);
+// });
+// server.listen(3000);
+// const { greeting, fullName } = require("./tester.js");
+//==============================================================================================
+const tester = require("./tester");
+// console.log(chalk.green(greeting("reza")));
+// console.log(chalk.white(fullName));
+// console.log(chalk.green("========"));
+// console.log(chalk.green("========"));
+// console.log(chalk.white("========"));
+// console.log(chalk.white("========"));
+// console.log(chalk.red("========"));
+// console.log(chalk.red("========"));
+// console.log(chalk.red("Iran"));
+// // ===========================================
+// console.log(process.pid);
+// console.log(process.arch);
+// process.argv.forEach((val, index) => {
+//   return console.log(`${index}: ${val}`);
+// });
+// console.log(process.config);
+// console.log(process.ppid);
+// console.log(process.title);
+// process.on("beforeExit", (code) => console.log(code));
+// console.log(process.env);
 
-
-
-console.log(chalk.green(greeting("reza")));
-console.log(chalk.white(fullName));
-console.log(chalk.green("========"));
-console.log(chalk.green("========"));
-console.log(chalk.white("========"));
-console.log(chalk.white("========"));
-console.log(chalk.red("========"));
-console.log(chalk.red("========"));
-console.log(chalk.red("Iran"));
-// ===========================================
-console.log(process.pid);
-console.log(process.arch);
-process.argv.forEach((val, index) => {
-    return console.log(`${index}: ${val}`);
-})
-console.log(process.config);
-console.log(process.ppid);
-console.log(process.title);
-process.on('beforeExit', (code) => console.log(code));
-console.log(process.env);
+tester.test1();
+tester.test2();
+console.log(process.argv);
--- tester.js
+++ tester.js
@@ -1,10 +1,19 @@
 const myName = "Ali";
 const sayHello = (arg) => {
-    return `Hello ${arg}`
-}
+  return `Hello ${arg}`;
+};
 
 // exports.greeting = sayHello;
 // exports.fullName = myName;
-
+const test1 = () => {
+  console.log("test 1 ==> exports.name=target");
+};
+const test2 = () => {
+  console.log("test 2 ==> module.exports={target}");
+};
 exports.greeting = sayHello;
 exports.fullName = myName;
+module.exports = {
+  test1,
+};
+module.exports.test2 = test2;
